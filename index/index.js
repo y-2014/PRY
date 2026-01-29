67341e7c779380930b6a8ebedd80bdba88e97b05
y-2014 <w.yoshino2014@gmail.com>
2026-01-29 14:37:03 +0900

    Delete index directory

--- index/index.js
+++
@@ -1,5 +0,0 @@
-var lastModifiedDate = new Date(document.lastModified);
-var year = lastModifiedDate.getFullYear();
-var month = lastModifiedDate.getMonth() + 1;
-var day = lastModifiedDate.getDate();
-document.write(year + "年" + month + "月" + day + "日");
